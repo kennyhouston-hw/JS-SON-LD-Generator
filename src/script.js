ba8973433305b4d2ea66c10abffb655d7a1df781
Kenny Houston <kennyhouston20@gmail.com>
2026-08-10 10:42:36 +0300

    Update author detection logic to support new author block structure

--- src/script.js
+++ src/script.js
@@ -202,18 +202,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // --- Логика определения автора ---
             const authorBlock = doc.querySelector('.t531');
-            const authorBlockNew = doc.querySelector('.uc-author-new');
+            const authorNameNew = doc.querySelector('.auth-name');
             if (authorBlock) {
                 // Блок найден, устанавливаем автора как Person
                 authorTypeInput.value = 'Person';
                 const authorNameElement = authorBlock.querySelector('.t531__title');
                 authorNameInput.value = authorNameElement ? authorNameElement.textContent.trim() : '';
                 authorUrlInput.value = 'https://hwschool.online/'; // Устанавливаем URL для Person
-            } else if (authorBlockNew) {
+            } else if (authorNameNew) {
                 // Найден новый блок автора, устанавливаем автора как Person
                 authorTypeInput.value = 'Person';
-                const authorNameElement = authorBlockNew.querySelector('.auth-name');
-                authorNameInput.value = authorNameElement ? authorNameElement.textContent.trim() : '';
+                authorNameInput.value = authorNameNew.textContent.trim();
                 authorUrlInput.value = 'https://hwschool.online/'; // Устанавливаем URL для Person
             } else {
                 // Блок не найден, устанавливаем автора как Organization
